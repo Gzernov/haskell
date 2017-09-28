5cfef06b8df66e506ceb2ae396eb5094ca9347b3
Gzernov <zer.gleb@gmail.com>
2017-09-29 02:01:31 +0300

    Homework 1
    - Removed shortest function

--- homework1.hs
+++ homework1.hs
@@ -6,10 +6,7 @@ order3 :: (Int, Int, Int) -> (Int, Int, Int)
 order3 (x, y, z) = let [a,b,c] = sort [x,y,z] in (c, b, a)
 
 highestBit :: Int -> (Int, Int)
--- handle not positive numbers
 highestBit x = let z = max x 0 in let y = max (finiteBitSize z - countLeadingZeros z - 1) 0 in (2 ^ y, y)
--- doesn't handle not positive numbers
--- highestBit x = let y = finiteBitSize x - countLeadingZeros x - 1 in (2 ^ y, y)
 
 smartReplicate :: [Int] -> [Int]
 smartReplicate = concatMap (\y -> replicate y y)
