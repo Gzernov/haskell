a86b7ce85cb0653a23d498d5258bf530fca08371
Gzernov <zer.gleb@gmail.com>
2017-10-29 01:34:11 +0300

    Block 2
    - Finished implementations (w/o tests)

--- homework2/src/Block2.hs
+++ homework2/src/Block2.hs
@@ -8,18 +8,22 @@ bin :: Int -> [[Int]]
 bin 0 = [[]]
 bin x = bin (x - 1) >>= (\elem -> [0 : elem, 1 : elem])
 
-combination :: Int -> Int -> [[Int]]
-combination n 1 = map (\x -> [x]) [1 .. n]
-combination n k = (combination n (k - 1)) >>= (\elem ->
-    filter (\x -> not $ null x)
+combinations :: Int -> Int -> [[Int]]
+combinations n 1 = map (: []) [1 .. n]
+combinations n k = combinations n (k - 1) >>= (\elem ->
+    filter (not . null)
       (map
         (\x ->
-         if x < head(elem)
+         if x < head elem
            then x:elem
            else [])
   [1 .. n - 1]))
 
 permutations :: [Int] -> [[Int]]
 permutations [x] = [[x]]
-permutations [x, y] = [[x, y], [y, x]]
-permutations list@(x:xs) = permutations(xs) >>= (\elem -> map (:elem) list)
+permutations list@(x:xs) = permutations xs >>= (\elem -> insert [] elem [])
+  where
+    insert :: [Int] -> [Int] -> [[Int]] -> [[Int]]
+    insert _head []    cur = (_head ++ [x]) : cur
+    insert _head _tail cur = insert (_head ++ [head _tail]) (tail _tail)
+      ((_head ++ x : _tail) : cur)
